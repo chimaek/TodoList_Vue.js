31cda29d2e81343ccba9ed01d1e69ff9fc2f7161
chimaek <pipiru100@gmail.com>
2021-04-27 13:36:54 +0900

    footer 추가

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,30 @@
 <template>
-<div class="text-center"> footer</div>  
+<div class='clearAllContainer'>
+    <span class='clearAllBtn' @click="clearTodo">전부삭제</span>
+</div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        clearTodo:function(){
+            localStorage.clear();
+        }
+    },
 }
 </script>
 
-<style>
-
+<style scoped>
+    .clearAllContainer{
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn{
+        color: #e20303;
+        display: block;
+    }
 </style>
